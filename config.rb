17b1de3ac4666a8bdb220db7e3a5b7bd05017a4c
sanzaru <iwuerstchen@gmail.com>
2015-04-04 15:38:20 +0200

    initial commit

---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "/"
+sass_dir = "src"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :compressed
+sourcemap = false
