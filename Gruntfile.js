31fb2040aa20c1b8a43426a79de7c899b030d2fb
sanzaru <iwuerstchen@gmail.com>
2015-04-04 20:04:43 +0200

    integrated grunt

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    compass: {
+      dist: {
+          options: {
+            banner: '/* <%= pkg.name %> v<%= pkg.version %> - <%= pkg.url %> */',
+            sassDir: 'src/',
+            cssDir: './',
+            outputStyle: 'compressed',
+            specify: 'src/*'
+          }
+      },
+    }, 
+
+    watch: {
+      compass: {
+        files: ['src/*.scss'],
+        tasks: ['compass']        
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['compass']);
+
+};
